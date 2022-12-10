a5a2ecaa85bbab25623b952868f5a233196048bc
Heriberto Robles Coronado <rrobleshc@hotmail.com>
2022-12-10 10:03:04 -0700

    SweetAlert2

---
+++ BlazorFarmacia/Client/Helpers/IJSExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorFarmacia.Client.Helpers
+{
+    public static class IJSExtensions
+    {
+        public static Task MostrarMensaje(this IJSRuntime js, string mensaje)
+        {
+            return js.InvokeAsync<object>("Swal.fire", mensaje);
+        }
+    }
+}
